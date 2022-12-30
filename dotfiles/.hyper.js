eed1c4e49e7498c398d89a5f914fa7f9b83d78d9
Suzen Fylke <codesue@users.noreply.github.com>
2022-12-29 22:38:38 -0500

    Inline hyper.js plugins

--- dotfiles/.hyper.js
+++ dotfiles/.hyper.js
@@ -1,9 +1,90 @@
 /**
  * Hyper Terminal Congiguration
  * See https://hyper.is#cfg for all currently supported options.
+ * Color palettes and CSS adapted from:
+ * - https://github.com/catppuccin/hyper/blob/main/index.js
+ * - https://github.com/lucleray/hyper-active-tab/blob/master/index.js
  */
 
-"use strict";
+'use strict';
+
+const palettes = {
+    catppuccinMocha: {
+        backgroundColor: '#1e1e2e',
+        foregroundColor: '#cdd6f4',
+        selectionColor: '#585b70',
+        cursorColor: '#f5e0dc',
+        cursorAccentColor: '#11111b',
+        colors: {
+            black: '#45475a',
+            blue: '#89b4fa',
+            red: '#f38ba8',
+            green: '#a6e3a1',
+            yellow: '#f9e2af',
+            magenta: '#f5c2e7',
+            cyan: '#94e2d5',
+            white: '#bac2de',
+            lightBlack: '#585b70',
+            lightWhite: '#a6adc8',
+            lightBlue: '#89b4fa',
+            lightRed: '#f38ba8',
+            lightGreen: '#a6e3a1',
+            lightYellow: '#f9e2af',
+            lightMagenta: '#f5c2e7',
+            lightCyan: '#94e2d5',
+        },
+      },
+}
+
+const theme = palettes.catppuccinMocha;
+const activeTabSymbol = '✨'
+
+const css = `
+    .hyper_main {
+        border: none !important;
+    }
+    .header_header {
+        color: ${theme.foregroundColor};
+    }
+        .header_windowHeaderWithBorder {
+        border-color: ${theme.backgroundColor} !important;
+    }
+    .tabs_borderShim {
+        border-color: transparent !important;
+    }
+    .tab_tab {
+        border: 0;
+    }
+    .tab_tab.tab_active {
+        background: ${theme.backgroundColor};
+        color: ${theme.foregroundColor};
+        transition: all 140ms ease;
+
+        span span:before {
+            content: '${activeTabSymbol} ';
+        }
+    }
+    .tab_tab:not(.tab_active) {
+        background: ${theme.colors.black};
+        color: ${theme.colors.lightWhite};
+    }
+    .tab_tab:not(.tab_active):hover {
+        background: ${theme.colors.lightBlack};
+        color: ${theme.foregroundColor};
+        transition: background 140ms ease;
+    }
+    .tab_icon {
+        width: 15px;
+        height: 15px;
+    }
+    .tab_shape {
+        width: 7px;
+        height: 7px;
+    }
+    .splitpane_divider {
+        background-color: ${theme.colors.lightBlack} !important;
+    }
+    `;
 
 module.exports = {
     config: {
@@ -46,10 +127,7 @@ module.exports = {
         fontSize: 12,
 
         // font family with optional fallbacks
-        fontFamily: 'Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
-
-        // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
-        cursorColor: 'rgba(248,28,229,0.8)',
+        fontFamily: 'Menlo, monospace',
 
         // whether to use a blinking cursor
         cursorBlink: false,
@@ -57,50 +135,33 @@ module.exports = {
         // 'BEAM' for |, 'UNDERLINE' for _, 'BLOCK' for █
         cursorShape: 'BLOCK',
 
+        // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
+        cursorColor: theme.cursorColor,
+
         // terminal text color under BLOCK cursor
-        cursorAccentColor: '#000',
+        cursorAccentColor: theme.cursorAccentColor,
 
         // terminal background color
-        backgroundColor: '#000',
+        backgroundColor: theme.backgroundColor,
 
         // terminal text color
-        foregroundColor: '#fff',
+        foregroundColor: theme.foregroundColor,
 
         // terminal selection color
-        selectionColor: 'rgba(248,28,229,0.3)',
+        selectionColor: theme.selectionColor,
 
         // border color for windows and tabs
-        borderColor: '#1e1e2e',
+        borderColor: theme.backgroundColor,
 
         // color palette
-        colors: {
-            black: '#000000',
-            red: '#C51E14',
-            green: '#1DC121',
-            yellow: '#C7C329',
-            blue: '#0A2FC4',
-            magenta: '#C839C5',
-            cyan: '#20C5C6',
-            white: '#C7C7C7',
-            lightBlack: '#686868',
-            lightRed: '#FD6F6B',
-            lightGreen: '#67F86F',
-            lightYellow: '#FFFA72',
-            lightBlue: '#6A76FB',
-            lightMagenta: '#FD7CFC',
-            lightCyan: '#68FDFE',
-            lightWhite: '#FFFFFF',
-            limeGreen: '#32CD32',
-            lightCoral: '#F08080',
-        },
+        colors: theme.colors,
 
-        // catppuccin theme (hypurr) variations:
-        // 'Latte', 'Frappé', 'Macchiato', or 'Mocha'
-        catppuccinTheme: 'Mocha'
+        // custom css
+        css: css,
 
     },
 
-    plugins: ['hypurr'],
+    plugins: [],
 };
 
 //# sourceMappingURL=config-default.js.map
